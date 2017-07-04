2bbb8dd91a38fa2c8674b34b748dad5db399a95b
prodotiscus <f.sizov@yandex.ru>
2017-07-04 10:23:44 +0300

    fix

--- selkup_alphabet.py
+++ selkup_alphabet.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-#
-# Coding (c) Fyodor Sizov, 2017
-# Alphabet specification (c) Grigoriy Korotkih, Fyodor Sizov, 2017
-#
-# The code is based on the Selkup.Tk alphabet specification and may be updated regularly:
-# http://selkup.tk/alphabet-specification
-#
-#
+
 class uni:
 	strict = [
 		[
@@ -24,18 +17,6 @@ class uni:
 		[
 			["а́̄"],
 			"ӓ̄"
-		],
-		[
-			["ю̈"],
-			"ӱ"
-		],
-		[
-			["ю̈̄"],
-			"ӱ̄"
-		],
-		[
-			["ӯ"],
-			"ю̄"
 		],
 		[
 			["i"],
