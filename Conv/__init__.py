12c42f5218f3f49c05ecac86aeae7a597f5997b0
prodotiscus <f.sizov@yandex.ru>
2017-09-30 21:39:19 +0300

    structure update

---
+++ Conv/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+from srds import *
+
+def short(string):
+    string = string.replace("̄", "")
+    string = string.replace("̈", "")
+    return string
+
+
+def unify(string, strict, soft, strict_only=False):
+    strict_result = string
+    for group in strict:
+        for query in group[0]:
+            strict_result = strict_result.replace(query, group[1])
+    #
+    soft_result = strict_result
+    for group in soft:
+        for query in group[0]:
+            soft_result = soft_result.replace(query, ''.join(["?" for _ in range(len(query))]))
+    if strict_only:
+        return strict_result
+    else:
+        return strict_result, soft_result
