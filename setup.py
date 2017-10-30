6035fa7a3ef6c98afe9f809975e405a1a86b20ff
prodotiscus <f.sizov@yandex.ru>
2017-10-30 17:46:21 +0300

    prepare data for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from distutils.core import setup
+setup(
+  name='selkup_alphabet',
+  packages=['selkup_alphabet'],
+  version='0.1',
+  description='Library that helps you to work with different Selkup scripts',
+  author='Fyodor Sizov',
+  author_email='f.sizov@yandex.ru',
+  url='https://github.com/selkup-community/selkup-alphabet',
+  download_url='https://github.com/selkup-community/selkup-alphabet/archive/0.1.tar.gz',
+  keywords=['testing', 'logging', 'example'],
+  classifiers=[],
+)
