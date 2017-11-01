8dd6ebf0c909a3ef729010ff5359351f81918960
Fyodor Sizov <f.sizov@yandex.ru>
2017-11-01 10:22:05 +0300

    run conv for titles

--- selkup_alphabet/Conv/__init__.py
+++ selkup_alphabet/Conv/__init__.py
@@ -15,6 +15,7 @@ def unify(string, strict, soft, strict_only=False):
     for group in strict:
         for query in group[0]:
             strict_result = strict_result.replace(query, group[1])
+            strict_result = strict_result.replace(query[0].upper() + query[1:], group[0].upper() + group[0][1:])
     #
     soft_result = strict_result
     for group in soft:
